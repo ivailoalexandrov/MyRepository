a543006bc1149e868f4913edd130e5de34da7b85
Ivailo Alexandrov <ivailo.alexandrov@icloud.com>
2019-11-09 11:47:24 +0200

    update

--- DefiningInstantiatingClasses.swift
+++ DefiningInstantiatingClasses.swift
@@ -2,21 +2,46 @@
 
 class Appliance {
 //    properties
-    var manufacturer: String = ""
-    var model: String = ""
-    var voltage: Int = 0
+    var manufacturer: String
+    var model: String
+    var voltage: Int
     var capacity: Int?
 
-//    methods
+//initializer
+    init() {
+        self.manufacturer = "default manufacturer"
+        self.model = "default model"
+        self.voltage = 120
+    }
+
+//additional initializer
+    init(withVoltage: Int) {
+        self.manufacturer = "default manufacturer"
+        self.model = "default model"
+        self.voltage = withVoltage
+    }
+
+//denitializer
+    deinit {
+        //perform cleanup code here...
+        //release a file resource...
+        //release a network resource...
+    }
+
+//methods
     func getDetails() -> String {
         var message = "This is the \(self.model) from \(self.manufacturer)."
         if self.voltage >= 220 {
-            message += "This model is for European usage."
+            message += " This model is for European usage."
         }
         return message
     }
 }
 
+var cafetiere = Appliance(withVoltage: 220)
+cafetiere.manufacturer = "Megappliance EU"
+cafetiere.model = "Electrotiere"
+print(cafetiere.getDetails())
 
 // ...later, create an instance of Appliance
 var kettle = Appliance()
